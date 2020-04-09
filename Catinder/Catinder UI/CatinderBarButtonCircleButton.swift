7dde2183d7e7dc579184b4cb5a34ca9bad197efc
Aleksey Paykachev <aleksey.paykachev@gmail.com>
2020-04-09 15:02:19 +0300

    Add CatinderBarButtonCircleButton.

---
+++ Catinder/Catinder UI/CatinderBarButtonCircleButton.swift
@@ -0,0 +1,43 @@
+//
+//  CatinderBarButtonCircleButton.swift
+//  Catinder
+//
+//  Created by Aleksey on 09.04.2020.
+//  Copyright © 2020 Aleksey Paykachev. All rights reserved.
+//
+
+import UIKit
+
+class CatinderBarButtonCircleButton: UIBarButtonItem {
+	
+	private let imageButton: CatinderCircleButton
+	private var clickEvent: (() -> ())?
+	
+	init(image: UIImage? = nil) {
+		imageButton = CatinderCircleButton(radius: 18, image: image)
+
+		super.init()
+		customView = imageButton
+		imageButton.addTarget(self, action: #selector(buttonDidPressed), for: .touchUpInside)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func set(image: UIImage?) {
+		imageButton.set(image: image)
+	}
+	
+	func set(imageName: String) {
+		imageButton.set(image: UIImage(named: imageName))
+	}
+	
+	func onClick(_ clickEvent: @escaping () -> ()) {
+		self.clickEvent = clickEvent
+	}
+	
+	@objc private func buttonDidPressed() {
+		clickEvent?()
+	}
+}
