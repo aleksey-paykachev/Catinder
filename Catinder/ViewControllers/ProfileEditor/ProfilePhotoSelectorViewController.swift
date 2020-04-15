5cceb7ed447ce9a43c2b511fd1ab678eb6a09752
Aleksey Paykachev <aleksey.paykachev@gmail.com>
2020-04-16 01:02:26 +0300

    Add complex compositional layout to ProfilePhotoSelectorViewController.

--- Catinder/ViewControllers/ProfileEditor/ProfilePhotoSelectorViewController.swift
+++ Catinder/ViewControllers/ProfileEditor/ProfilePhotoSelectorViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ProfilePhotoSelectorViewController: UICollectionViewController {
 	
 	// demo images instead of user photos
-	var images = ["person.badge.plus.fill", "person.badge.plus", "person.crop.circle.badge.plus", "person.crop.circle.fill.badge.plus", "plus.rectangle", "plus.rectangle.fill", "plus.square", "plus.square.fill", "plus.circle", "plus.circle.fill", "photo", "photo.fill", "photo.on.rectangle", "photo.fill.on.rectangle.fill", "car", "airplane", "printer", "gamecontroller", "folder", "paperplane", "bell"].map { UIImage(systemName: $0)! }
+	var images = ["person.badge.plus.fill", "person.crop.circle.badge.plus", "person.crop.circle.fill.badge.plus", "plus.square", "plus.square.fill", "plus.circle", "plus.circle.fill", "photo", "photo.fill", "photo.on.rectangle", "photo.fill.on.rectangle.fill"].map { UIImage(systemName: $0)! }
 	
-	private let layout = UICollectionViewFlowLayout()
+	private let layout = createLayout()
 	
 	init() {
 		super.init(collectionViewLayout: layout)
@@ -37,11 +37,48 @@ class ProfilePhotoSelectorViewController: UICollectionViewController {
 		
 		collectionView.backgroundColor = .clear
 		collectionView.register(ProfilePhotoSelectorCell.self, forCellWithReuseIdentifier: "ProfilePhotoSelectorCell")
-		layout.itemSize = CGSize(width: 100, height: 100)
 	}
 	
+	private static func createLayout() -> UICollectionViewCompositionalLayout {
+		// large item (main top-left item)
+		let largeItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.66), heightDimension: .fractionalHeight(1))
+		let largeItem = NSCollectionLayoutItem(layoutSize: largeItemSize)
+
+		// small vertical items and group (right items)
+		let smallVerticalItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5))
+		let smallVerticalItem = NSCollectionLayoutItem(layoutSize: smallVerticalItemSize)
+		
+		let verticalGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalHeight(1))
+		let verticalGroup = NSCollectionLayoutGroup.vertical(layoutSize: verticalGroupSize, subitem: smallVerticalItem, count: 2)
+
+		// top group (main item + right items)
+		let topGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.66))
+		let topGroup = NSCollectionLayoutGroup.horizontal(layoutSize: topGroupSize, subitems: [largeItem, verticalGroup])
+		
+		// small horizontal items and group (bottom items)
+		let smallHorizontalItemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalHeight(1))
+		let smallHorizontalItem = NSCollectionLayoutItem(layoutSize: smallHorizontalItemSize)
+		
+		let horizontalGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.99), heightDimension: .fractionalHeight(0.33))
+		let horizontalGroup = NSCollectionLayoutGroup.horizontal(layoutSize: horizontalGroupSize, subitem: smallHorizontalItem, count: 3)
+		
+		// main group
+		let mainGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1))
+		let mainGroup = NSCollectionLayoutGroup.vertical(layoutSize: mainGroupSize, subitems: [topGroup, horizontalGroup])
+
+		let section = NSCollectionLayoutSection(group: mainGroup)
+		
+		return UICollectionViewCompositionalLayout(section: section)
+	}
+}
+
+
+// MARK: - UICollectionViewDataSource
+
+extension ProfilePhotoSelectorViewController {
+
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		images.count
+		6
 	}
 	
 	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -50,7 +87,13 @@ class ProfilePhotoSelectorViewController: UICollectionViewController {
 		cell.set(image: images[indexPath.item])
 		return cell
 	}
-	
+}
+
+
+// MARK: - UICollectionViewDelegate
+
+extension ProfilePhotoSelectorViewController {
+
 	override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
 		print(indexPath)
 		let photoImagePicker = PhotoImagePicker()
